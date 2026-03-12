226d91b4be3a7af1e747b565710aa1f7164005b6
nshib00 <nshib.54321@gmail.com>
2026-03-12 23:55:30 +0300

    Add middleware, update main app file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,25 +7,29 @@ import (
 	"os"
 	"tasks-api/internal/database"
 	"tasks-api/internal/handlers"
+	"tasks-api/internal/middleware"
 )
 
-func main() {
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
-		log.Fatal("error: some of env variables are not set\n")
-		os.Exit(1)
+func getFromEnv(varName string) string {
+	envVar := os.Getenv(varName)
+	if envVar == "" {
+		log.Fatalf("error: env variable does not exist or not set: %s", varName)
 	}
+	return envVar
+}
+
+func main() {
+	dbUser := getFromEnv("POSTGRES_USER")
+	dbPassword := getFromEnv("POSTGRES_PASSWORD")
+	dbHost := getFromEnv("POSTGRES_HOST")
+	dbPort := getFromEnv("POSTGRES_PORT")
+	dbName := getFromEnv("POSTGRES_NAME")
+	serverPort := getFromEnv("SERVER_PORT")
 
 	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	db, err := database.Connect(dbURL)
 	if err != nil {
 		log.Fatal("database connection error:", err)
-		return
 	}
 	defer db.Close()
 	log.Println("connected to database successfully")
@@ -37,6 +41,13 @@ func main() {
 	mux.HandleFunc("/api/tasks", taskMethodHandler(handler.GetAllTasks, http.MethodGet))
 	mux.HandleFunc("/api/tasks/create", taskMethodHandler(handler.CreateTask, http.MethodPost))
 	mux.HandleFunc("/api/tasks/", taskMethodWithIDHandler(handler))
+
+	loggedMux := middleware.LoggingMiddleware(mux)
+	serverAddr := ":" + serverPort
+	if err := http.ListenAndServe(serverAddr, loggedMux); err != nil {
+		log.Fatal("server error: ", err)
+	}
+	log.Printf("server started on %s", serverAddr)
 }
 
 func taskMethodHandler(handlerFunc http.HandlerFunc, allowedMethod string) http.HandlerFunc {
